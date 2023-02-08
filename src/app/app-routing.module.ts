c0638090b507cb2edb159ad1ca75d737f5f3dd48
MohamedZouh <diversinfo8086@gmail.com>
2023-02-08 10:46:19 +0100

    app completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { ContactComponent } from './contact/contact.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:"contact", component:ContactComponent},
+    { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+    { path: '', pathMatch: 'full', redirectTo: 'articles' },
+    { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
+    { path: '**', redirectTo: 'articles' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
+import { ContactModule } from './contact/contact.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ScullyLibModule } from '@scullyio/ng-lib';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ContactModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
